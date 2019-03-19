04c5fc66b0189bc625f22f80158ea4654c51dd60
Vlad Mashkin <vlad.mashkin@anvileight.com>
2019-03-19 17:00:02 +0200

    Add login to the browsable API

--- snippets/views.py
+++ snippets/views.py
@@ -1,13 +1,32 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework import permissions
 
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
+
+User = get_user_model()
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 # CBV with mixins
@@ -39,6 +58,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
 
 
 # CBV with mixins
